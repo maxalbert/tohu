4c52fee7b4d32e7c960f8243bbe553dc3d3cc6cf
Maximilian Albert <maximilian.albert@gmail.com>
2017-09-18 00:13:07 +0100

    Add pandas as dependency.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(),
-    install_requires=['mako', 'python-dateutil'],
+    install_requires=['mako', 'pandas', 'python-dateutil'],
     extras_require={
         'dev': ['ipython', 'jupyter'],
         'test': ['pytest', 'nbval'],
