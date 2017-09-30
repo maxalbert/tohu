156d8c3a42d57bdd256ead0bfcc70945c840cfbc
Maximilian Albert <maximilian.albert@gmail.com>
2017-09-30 10:59:41 +0100

    Add support for writing CSV output.

--- tests/test_item_collection.py
+++ tests/test_item_collection.py
@@ -2,9 +2,14 @@
 Tests for the ItemCollection class.
 """
 
+import os
 import pytest
+import textwrap
+
 from .context import tohu
 from tohu.item_collection import ItemCollection
+from tohu.custom_generator import CustomGenerator
+from tohu.generators import ChooseFrom, HashDigest, Integer, Sequential, Timestamp
 
 
 class TestItemCollection:
@@ -27,3 +32,45 @@ class TestItemCollection:
         assert c[1] == "world"
         assert c[2] == "foobar"
         assert c[3] == "quux"
+
+    def test_write_csv(self, tmpdir):
+        filename = tmpdir.join("output.csv").strpath
+
+        class FoobarGenerator(CustomGenerator):
+            aaa = HashDigest(length=6)
+            bbb = Sequential(prefix="Foo_", digits=2)
+            ccc = Integer(0, 100)
+
+        class QuuxGenerator(CustomGenerator):
+            def __init__(self, foo_items):
+                self.foo1 = ChooseFrom(foo_items)
+                self.foo2 = ChooseFrom(foo_items)
+                self.date_str = Timestamp(start="2006-01-01", end="2017-09-01", fmt='%d-%b-%y')
+
+        foo = FoobarGenerator()
+        foo_items = foo.generate(10, seed=12345)
+        quux = QuuxGenerator(foo_items)
+        quux_items = quux.generate(5, seed=99999)
+
+        csv_fields = {
+            'Column_1': 'foo1.aaa',
+            'Column_2': 'foo1.bbb',
+            'Column_3': 'foo2.aaa',
+            'Column_4': 'foo2.ccc',
+            'Column_5': 'date_str',
+        }
+
+        assert not os.path.exists(filename)
+        quux_items.write(filename, fields=csv_fields)
+        assert os.path.exists(filename)
+
+        expected_output = textwrap.dedent("""\
+            #Column_1,Column_2,Column_3,Column_4,Column_5
+            27E995,Foo_04,27E995,58,25-Sep-10
+            27E995,Foo_04,9CB45F,36,12-Aug-11
+            9CB45F,Foo_10,297F9D,39,17-Jan-07
+            1A2721,Foo_06,B2A827,1,05-Jan-16
+            F5C771,Foo_08,8CE1AD,68,09-Mar-12
+            """)
+
+        assert open(filename).read() == expected_output
--- tohu/item_collection.py
+++ tohu/item_collection.py
@@ -1,3 +1,25 @@
+from mako.template import Template
+
+__all__ = ['ItemCollection']
+
+
+def wrap_field_for_mako(fld):
+    """
+    Helper function which wraps a field name such as 'foo'
+    into the mako expression '${item.foo}'.
+    """
+    return "${item." + fld + "}"
+
+
+def make_mako_template(fields, *, sep=",", line_separator="\n"):
+    """
+    Helper function. Returns a mako Template which renders
+    an individual item to a string.
+    """
+    field_strings = [wrap_field_for_mako(x) for x in fields.values()]
+    return Template(sep.join(field_strings) + line_separator)
+
+
 class ItemCollection:
     """
     Class representing a collection of items.
@@ -17,3 +39,27 @@ class ItemCollection:
 
     def __getitem__(self, idx):
         return self.items[idx]
+
+    def write(self, filename, fields, sep=",", line_separator="\n"):
+        """
+        Write item collection to CSV file.
+
+        Parameters
+        ----------
+        filename: string
+            Name of the output CSV file.
+        fields: dict
+            Dictionary of the form {<column_name>: <field_name>, ...}
+        sep: string
+            Field separator. Default: ','
+        line_separator: string
+            Line separatore. Default: '\n' (= newline).
+        """
+
+        template = make_mako_template(fields, sep=sep, line_separator=line_separator)
+        header_line = "#" + sep.join(fields.keys()) + line_separator
+
+        with open(filename, 'w') as f:
+            f.write(header_line)
+            for item in self.items:
+                f.write(template.render(item=item))
