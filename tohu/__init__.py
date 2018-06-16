20bd8b8d7bcb32a4cf24ee0f49500fe2e96a076e
Maximilian Albert <maximilian.albert@gmail.com>
2018-06-16 20:05:38 +0100

    Add logger

--- tohu/__init__.py
+++ tohu/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from .generators import *
 from .custom_generator import CustomGenerator
 from . import generators
@@ -8,3 +10,15 @@ __all__ = generators.__all__ + custom_generator.__all__
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+#
+# Create logger
+#
+logger = logging.getLogger('tohu')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('{asctime} {levelname}  {message}', datefmt='%Y-%m-%d %H:%M:%S', style='{')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
