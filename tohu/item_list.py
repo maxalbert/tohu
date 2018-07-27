4da7173b2fa37f9abc9000c196c7d74f1d167f6e
Maximilian Albert <maximilian.albert@gmail.com>
2018-07-27 19:33:09 +0100

    Add docstring; use boolean argument 'append' instead of 'mode'

--- tohu/item_list.py
+++ tohu/item_list.py
@@ -30,7 +30,29 @@ class ItemList:
             attr_getters = [(field_name, attrgetter(attr_name)) for (field_name, attr_name) in fields.items()]
             return pd.DataFrame([pd.Series({field_name: func(x) for (field_name, func) in attr_getters}) for x in self.items])
 
-    def to_csv(self, filename, *, fields, mode='w', sep=',', header=True, header_prefix='#'):
+    def to_csv(self, filename, *, fields, append=False, sep=',', header=True, header_prefix='#'):
+        """
+        Parameters
+        ----------
+        fields: dict
+            Dictionary which maps output field names to attribute names of the generators.
+            Example: `fields={'CSV_COL1': 'field_name_1', 'CSV_COL2': 'field_name_2'}
+        append: bool
+            If `True`, open the file in 'append' mode to avoid overwriting existing content.
+            Default is `False`, i.e. any existing content will be overwritten.
+        sep: str
+            Field separator to use in the output. Default: ','
+        header: bool or str or None
+            If `header=False` or `header=None` then no header line will be written.
+            If `header` is a string then this string will be used as the header line.
+            If `header=True` then a header line will be automatically generated from
+            the field names of the custom generator.
+        header_prefix: str
+            If `header=True` then the auto-generated header line will be prefixed
+            with `header_prefix` (otherwise this argument has no effect).
+        """
+        assert isinstance(append, bool)
+
         header_names = [name for name in fields.keys()]
         attr_getters = [attrgetter(attr_name) for attr_name in fields.values()]
         newline = '\n'
@@ -46,7 +68,7 @@ class ItemList:
                 else:
                     header_line = ""
 
-        with open(filename, mode) as f:
+        with open(filename, 'a' if append else 'w') as f:
             f.write(header_line)
 
             for x in self.items:
