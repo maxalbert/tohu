fe397e91926e50eb738eb3db862aea5ff97ca737
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-01 10:35:47 +0100

    First stab at finding field generators in class dict and instance dict (note that generators in the instance dict are not found yet)

--- tohu/custom_generator_v2.py
+++ tohu/custom_generator_v2.py
@@ -1,6 +1,27 @@
 import logging
+from .generators import BaseGenerator
 
 logger = logging.getLogger('tohu')
+
+
+def find_field_generators(obj):
+    cls_dict = obj.__class__.__dict__
+    inst_dict = obj.__dict__
+    logger.debug(f'[FFF] cls_dict={cls_dict}')
+    logger.debug(f'[FFF] inst_dict={inst_dict}')
+
+    field_gens = {}
+
+    for name, gen in cls_dict.items():
+        if isinstance(gen, BaseGenerator):
+            field_gens[name] = gen
+
+    for name, gen in inst_dict.items():
+        if isinstance(gen, BaseGenerator):
+            field_gens[name] = gen
+
+    #return {name: gen for name, gen in cls_and_inst_dict.items() if isinstance(gen, BaseGenerator)}
+    return field_gens
 
 
 class CustomGeneratorMetaV2(type):
@@ -18,6 +39,15 @@ class CustomGeneratorMetaV2(type):
         #
         new_obj = super(CustomGeneratorMetaV2, metacls).__new__(metacls, cg_name, bases, clsdict)
         logger.debug(f'   - new_obj={new_obj}')
+
+        #
+        # Find field generators
+        #
+        field_gens = find_field_generators(new_obj)
+        logger.debug(f'Found {len(field_gens)} field generator(s):')
+        for name, gen in field_gens.items():
+            logger.debug(f'   - {name}: {gen}')
+
 
         #
         # Create and assign automatically generated reset() method
