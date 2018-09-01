72dd3f7245f07f7c596da707dffbb67ae6aa7cd0
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-01 10:33:33 +0100

    Tweak debugging messages, add comments

--- tohu/custom_generator_v2.py
+++ tohu/custom_generator_v2.py
@@ -6,14 +6,22 @@ logger = logging.getLogger('tohu')
 class CustomGeneratorMetaV2(type):
 
     def __new__(metacls, cg_name, bases, clsdict):
-        logger.debug('[DDD] CustomGeneratorMetaV2.__new__')
-        logger.debug(f'         - metacls={metacls}')
-        logger.debug(f'         - cg_name={cg_name}')
-        logger.debug(f'         - bases={bases}')
-        logger.debug(f'         - clsdict={clsdict}')
+        logger.debug('[DDD]')
+        logger.debug('CustomGeneratorMetaV2.__new__')
+        logger.debug(f'   - metacls={metacls}')
+        logger.debug(f'   - cg_name={cg_name}')
+        logger.debug(f'   - bases={bases}')
+        logger.debug(f'   - clsdict={clsdict}')
 
+        #
+        # Create new custom generator object
+        #
         new_obj = super(CustomGeneratorMetaV2, metacls).__new__(metacls, cg_name, bases, clsdict)
-        logger.debug(f'         -- new_obj={new_obj}')
+        logger.debug(f'   - new_obj={new_obj}')
+
+        #
+        # Create and assign automatically generated reset() method
+        #
 
         def reset(self, seed=None):
             logger.debug(f'[EEE] Inside automatically generated reset() method for {self} (seed={seed})')
