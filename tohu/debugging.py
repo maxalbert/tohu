527b3bea1e8e14d2a7f7b65ebca3a25ba524b872
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-01 12:01:58 +0100

    Add DummyGenerator to debugging.py

--- tohu/debugging.py
+++ tohu/debugging.py
@@ -1,4 +1,5 @@
 import logging
+from .generators import BaseGenerator
 
 #
 # Create debug logger
@@ -18,3 +19,30 @@ def debug_print_dict(d, name=None):
         logger.debug(f'{name}:')
     for name, gen in d.items():
         logger.debug(f'   {name}: {gen}')
+
+
+class DummyGenerator(BaseGenerator):
+    """
+    Dummy generator independent of all existing generators
+    which we can add to a CustomGenerator v2.
+    """
+
+    def __init__(self, name, spawned_from=None):
+        self.cnt = 0
+        self.name = name
+        self.spawned_from = spawned_from
+
+    def __repr__(self):
+        descr = f' (id={id(self)})' if self.spawned_from is None else f' (spawned from {id(self.spawned_from)})'
+        return f"<DummyGenerator: '{self.name}'{descr}>"
+
+    def _spawn(self):
+        return DummyGenerator(self.name, spawned_from=self)
+
+    def reset(self, seed=None):
+        self.cnt = 0
+
+    def __next__(self):
+        value = self.cnt
+        self.cnt += 1
+        return f"<dummy_value: {value}>"
