9b01886715655fb119e5b4fa9525acd1e25b41d6
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-01 16:12:24 +0100

    Ensure that item class is a mapping

--- tohu/custom_generator.py
+++ tohu/custom_generator.py
@@ -94,6 +94,7 @@ def make_item_class(clsname, attr_names):
 
     item_cls.__repr__ = new_repr
     item_cls.__eq__ = new_eq
+    item_cls.keys = lambda self: attr_names
     item_cls.__getitem__ = lambda self, key: getattr(self, key)
     item_cls.as_dict = lambda self: attr.asdict(self)
     item_cls.to_series = lambda self: pd.Series(attr.asdict(self))
