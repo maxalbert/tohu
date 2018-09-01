2d0b530c437c2dfa07ae5d586984c912743e7031
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-01 10:59:52 +0100

    Add debug logger which omits timestamps (for easier diff-ing of the output)

--- tohu/custom_generator_v2.py
+++ tohu/custom_generator_v2.py
@@ -1,7 +1,18 @@
 import logging
 from .generators import BaseGenerator
 
-logger = logging.getLogger('tohu')
+#
+# Create debug logger
+#
+logger = logging.getLogger('tohu-debug')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('{levelname}  {message}', datefmt='%Y-%m-%d %H:%M:%S', style='{')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel('DEBUG')
+
 
 def debug_print_dict(d, name=None):
     if name is not None:
