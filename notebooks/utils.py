bc36a19d3bb1c07cbe2a44de88f227ef71c50b8c
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-02 09:52:48 +0100

    Allow passing seed directly to helper function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,8 +1,10 @@
-def print_generated_sequence(g, num, *, sep=", "):
+def print_generated_sequence(g, num, *, sep=", ", seed=None):
     """
     Helper function which prints a sequence of `num` items
     produced by the random generator `g`.
     """
+    if seed:
+       g.reset(seed)
 
     elems = [str(next(g)) for _ in range(num)]
     sep_initial = "\n" if sep == "\n" else " "
