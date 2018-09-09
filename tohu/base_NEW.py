d3dc89166c142bbb3abb36b9ea0d0372ec73d080
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-09 11:33:24 +0100

    Add SeedGenerator

--- tohu/base_NEW.py
+++ tohu/base_NEW.py
@@ -1,7 +1,37 @@
 from itertools import islice
+from random import Random
 from tqdm import tqdm
 
 from .item_list import ItemList
+
+
+class SeedGenerator:
+    """
+    This class is used in custom generators to create a collection of
+    seeds when reset() is called, so that each of the constituent
+    generators can be re-initialised with a different seed in a
+    reproducible way.
+
+    Note: This is almost identical to the `tohu.Integer` generator, but we
+    need a version which does *not* inherit from `TohuUltraBaseGenerator`,
+    otherwise the automatic item class creation in `CustomGeneratorMeta`
+    gets confused.
+    """
+
+    def __init__(self):
+        self.randgen = Random()
+        self.minval = 0
+        self.maxval = 2**32 - 1
+
+    def reset(self, seed):
+        self.randgen.seed(seed)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.randgen.randint(self.minval, self.maxval)
+
 
 #
 # TODO: Make `TohuUltraBaseGenerator` an abstract base class to ensure that the expected API methods are implemented.
