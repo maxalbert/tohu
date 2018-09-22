44c529294b7e6465691fef041a1e47c11488e791
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-22 18:20:47 +0100

    Add HashDigest

--- tohu/v3/primitive_generators.py
+++ tohu/v3/primitive_generators.py
@@ -1,6 +1,8 @@
+import numpy as np
 from random import Random
+from ..utils import identity
 
-__all__ = ['Constant', 'Integer']
+__all__ = ['Constant', 'HashDigest', 'Integer']
 
 
 class Constant:
@@ -48,3 +50,43 @@ class Integer:
 
     def __next__(self):
         return self.randgen.randint(self.low, self.high)
+
+
+class HashDigest:
+    """
+    Generator which produces a sequence of hex strings representing hash digest values.
+    """
+
+    def __init__(self, *, length=None, as_bytes=False, uppercase=True):
+        """
+        Parameters
+        ----------
+        length: integer
+            Length of the character strings produced by this generator.
+        as_bytes: bool
+            If True, return `length` random bytes. If False, return a string of `length`
+            characters with a hexadecimal representation of `length/2` random bytes.
+            Note that in the second case `length` must be an even number.
+        uppercase: bool
+            If True (the default), return hex string using uppercase letters, otherwise lowercase.
+            This only has an effect if `as_bytes=False`.
+        """
+        self.length = length
+        self._internal_length = length if as_bytes else length / 2
+        if not as_bytes and (length % 2) != 0:
+            raise ValueError(
+                f"Length must be an even number if as_bytes=False because it "
+                f"represents length = 2 * num_random_bytes. Got: length={length})")
+        self.as_bytes = as_bytes
+        self.uppercase = uppercase
+        self.randgen = np.random.RandomState()
+        self._maybe_convert_to_hex = identity if self.as_bytes else bytes.hex
+        self._maybe_convert_to_uppercase = identity if (self.as_bytes or not uppercase) else str.upper
+
+    def reset(self, seed):
+        self.randgen.seed(seed)
+        return self
+
+    def __next__(self):
+        val = self.randgen.bytes(self._internal_length)
+        return self._maybe_convert_to_uppercase(self._maybe_convert_to_hex(val))
