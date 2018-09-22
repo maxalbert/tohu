280219f9f8a8c9cda246511898f0df805e123d5e
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-22 23:38:35 +0100

    Add common base class for primitive generators

--- tohu/v3/primitive_generators.py
+++ tohu/v3/primitive_generators.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 import numpy as np
 from faker import Faker
 from random import Random
@@ -9,7 +10,27 @@ PRIMITIVE_GENERATORS = ['Constant', 'FakerGenerator', 'HashDigest', 'Integer', '
 __all__ = PRIMITIVE_GENERATORS + ['PRIMITIVE_GENERATORS']
 
 
-class Constant:
+class PrimitiveGenerator(metaclass=ABCMeta):
+    """
+    Base class for all primitive generators
+    """
+    def __init__(self):
+        self._clones = []
+
+    def register_clone(self, clone):
+        self._clones.append(clone)
+
+    @abstractmethod
+    def __next__(self):
+        raise NotImplementedError("Class {} does not implement method '__next__'.".format(self.__class__.__name__))
+
+    @abstractmethod
+    def reset(self, seed):
+        for c in self._clones:
+            c.reset(seed)
+
+
+class Constant(PrimitiveGenerator):
     """
     Generator which produces a constant sequence (repeating the same value indefinitely).
     """
@@ -21,16 +42,18 @@ class Constant:
         value:
             The constant value produced by this generator.
         """
+        super().__init__()
         self.value = value
 
     def reset(self, seed=None):
+        super().reset(seed)
         return self
 
     def __next__(self):
         return self.value
 
 
-class Integer:
+class Integer(PrimitiveGenerator):
     """
     Generator which produces random integers k in the range low <= k <= high.
     """
@@ -50,9 +73,8 @@ class Integer:
         self._clones = []
 
     def reset(self, seed):
+        super().reset(seed)
         self.randgen.seed(seed)
-        for c in self._clones:
-            c.reset(seed)
         return self
 
     def __next__(self):
@@ -62,7 +84,7 @@ class Integer:
         self._clones.append(clone)
 
 
-class HashDigest:
+class HashDigest(PrimitiveGenerator):
     """
     Generator which produces a sequence of hex strings representing hash digest values.
     """
@@ -81,6 +103,7 @@ class HashDigest:
             If True (the default), return hex string using uppercase letters, otherwise lowercase.
             This only has an effect if `as_bytes=False`.
         """
+        super().__init__()
         self.length = length
         self._internal_length = length if as_bytes else length / 2
         if not as_bytes and (length % 2) != 0:
@@ -94,6 +117,7 @@ class HashDigest:
         self._maybe_convert_to_uppercase = identity if (self.as_bytes or not uppercase) else str.upper
 
     def reset(self, seed):
+        super().reset(seed)
         self.randgen.seed(seed)
         return self
 
@@ -102,7 +126,7 @@ class HashDigest:
         return self._maybe_convert_to_uppercase(self._maybe_convert_to_hex(val))
 
 
-class FakerGenerator:
+class FakerGenerator(PrimitiveGenerator):
     """
     Generator which produces random elements using one of the methods supported by faker. [1]
 
@@ -124,6 +148,7 @@ class FakerGenerator:
         ----------
         [1] https://faker.readthedocs.io/
         """
+        super().__init__()
         self.method = method
         self.locale = locale
         self.faker_args = faker_args
@@ -132,6 +157,7 @@ class FakerGenerator:
         self.randgen = getattr(self.fake, method)
 
     def reset(self, seed):
+        super().reset(seed)
         self.fake.seed_instance(seed)
         return self
 
@@ -139,7 +165,7 @@ class FakerGenerator:
         return self.randgen(**self.faker_args)
 
 
-class IterateOver:
+class IterateOver(PrimitiveGenerator):
     """
     Generator which simply iterates over all items in a given iterable
     """
@@ -147,6 +173,7 @@ class IterateOver:
     def __init__(self, seq):
         assert isinstance(seq, (list, tuple, ItemList, str)), \
             "For the time being we enforce g being a list, tuple, ItemList or string so that we can spawn and reset this generator."
+        super().__init__()
         self.seq = seq
         self.gen = None
         self.reset()
@@ -161,11 +188,12 @@ class IterateOver:
         return self
 
     def reset(self, seed=None):
+        super().reset(seed)
         self.gen = iter(self.seq)
         return self
 
 
-class SelectOne:
+class SelectOne(PrimitiveGenerator):
     """
     Generator which produces a sequence of items taken from a given set of elements.
     """
@@ -180,6 +208,7 @@ class SelectOne:
             The probabilities associated with each element in `values`.
             If not given the assumes a uniform distribution over all values.
         """
+        super().__init__()
         self.values = values
         self.p = p
         self.randgen = np.random.RandomState()
@@ -190,5 +219,6 @@ class SelectOne:
         return self.values[idx]
 
     def reset(self, seed):
+        super().reset(seed)
         self.randgen.seed(seed)
         return self
