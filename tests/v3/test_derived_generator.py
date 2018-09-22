9d07685d1fe6198a31890a35ca822ce70a4e79c1
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-23 00:42:33 +0100

    Add test for chained Apply generators

--- tests/v3/test_derived_generator.py
+++ tests/v3/test_derived_generator.py
@@ -3,10 +3,11 @@ from tohu.v3.primitive_generators import Integer
 from tohu.v3.derived_generators import *
 
 
-def test_apply_sum_operator():
-    def add(x, y):
-        return x + y
+def add(x, y):
+    return x + y
 
+
+def test_apply_sum_operator():
     g1 = Integer(100, 200)
     g2 = Integer(30, 40)
     h = Apply(add, g1, g2)
@@ -25,3 +26,24 @@ def test_apply_sum_operator():
 
     assert items_h == items_summed
     assert items_h_after == items_summed_after
+
+
+def test_chained_apply_sum_operator():
+    g1 = Integer(100, 200)
+    g2 = Integer(300, 400)
+    g3 = Integer(500, 600)
+    g4 = Integer(700, 800)
+
+    h5 = Apply(add, g1, g2)
+    h6 = Apply(add, g3, g4)
+    h7 = Apply(add, h5, h6)
+
+    items1 = list(g1.generate(num=10, seed=11111))
+    items2 = list(g2.generate(num=10, seed=22222))
+    items3 = list(g3.generate(num=10, seed=33333))
+    items4 = list(g4.generate(num=10, seed=44444))
+    items_summed = [a+b+c+d for (a, b, c, d) in zip(items1, items2, items3, items4)]
+
+    items7 = list(h7.generate(num=10))
+
+    assert items7 == items_summed
