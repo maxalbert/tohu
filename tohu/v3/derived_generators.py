8ed899b0e3f739b71dda08b830a758ca64948fa7
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-23 21:51:58 +0100

    Tweak for easier reading (same functionality)

--- tohu/v3/derived_generators.py
+++ tohu/v3/derived_generators.py
@@ -16,8 +16,8 @@ class Apply(TohuBaseGenerator):
         self.orig_arg_gens = list(arg_gens)
         self.orig_kwarg_gens = kwarg_gens
 
-        self.arg_gens = [g.clone() for g in arg_gens]
-        self.kwarg_gens = {name: g.clone() for name, g in kwarg_gens.items()}
+        self.arg_gens = [g.clone() for g in self.orig_arg_gens]
+        self.kwarg_gens = {name: g.clone() for name, g in self.orig_kwarg_gens.items()}
 
     def __next__(self):
         next_args = (next(g) for g in self.arg_gens)
