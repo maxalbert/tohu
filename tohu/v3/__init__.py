800e84c5548a32013642055981a7ecf0daf8b683
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-30 22:39:52 +0100

    Remove old files

--- tohu/v3/__init__.py
+++
@@ -1,8 +0,0 @@
-from .primitive_generators import *
-from .derived_generators import *
-from . import primitive_generators
-from . import derived_generators
-
-ALL_GENERATORS = PRIMITIVE_GENERATORS + DERIVED_GENERATORS
-
-__all__ = primitive_generators.__all__ + derived_generators.__all__ + ['ALL_GENERATORS']
