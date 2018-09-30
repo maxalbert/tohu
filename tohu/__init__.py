9e0b022902ca67060ea890b4b52c69d8098f4c40
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-30 21:37:22 +0100

    Avoid creating two loggers with the same name (to avoid duplicate messages)

--- tohu/__init__.py
+++ tohu/__init__.py
@@ -1,9 +1,8 @@
-import logging
-
 from .base import *
 from .generators import *
 from .derived_generators import *
 from .custom_generator import CustomGenerator
+from .v4.logging import logger
 from . import generators
 from . import custom_generator
 from . import derived_generators
@@ -13,17 +12,3 @@ __all__ = base.__all__ + generators.__all__ + derived_generators.__all__ + custo
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-
-#
-# Create logger
-#
-logger = logging.getLogger('tohu')
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('{asctime} {levelname}  {message}', datefmt='%Y-%m-%d %H:%M:%S', style='{')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-tohu_logger = logger  # alias
