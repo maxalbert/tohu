ab1cf189c432e750485c32ea35321b37f84cd1d4
Maximilian Albert <maximilian.albert@gmail.com>
2018-09-30 22:59:35 +0100

    Import latest version at toplevel by default

--- tohu/__init__.py
+++ tohu/__init__.py
@@ -1,14 +1,17 @@
-from .base import *
-from .generators import *
-from .derived_generators import *
-from .custom_generator import CustomGenerator
+from .v4.base import *
+from .v4.primitive_generators import *
+from .v4.derived_generators import *
+from .v4.custom_generator import CustomGenerator
 from .v4.logging import logger
-from . import generators
-from . import custom_generator
-from . import derived_generators
+from .v4 import base
+from .v4 import primitive_generators
+from .v4 import custom_generator
+from .v4 import derived_generators
 
-__all__ = base.__all__ + generators.__all__ + derived_generators.__all__ + custom_generator.__all__ + ['tohu_logger']
+__all__ = base.__all__ + primitive_generators.__all__ + derived_generators.__all__ + custom_generator.__all__ + ['tohu_logger']
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+tohu_logger = logger  # alias
