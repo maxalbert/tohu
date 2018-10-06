8de566d51f72876fd200cf4856a9095d95b8cd8a
Maximilian Albert <maximilian.albert@gmail.com>
2018-10-06 14:10:39 +0100

    Set a few special methods to allow adding and multiplying tohu generators and comparing them

--- tohu/__init__.py
+++ tohu/__init__.py
@@ -7,6 +7,7 @@ from .v4 import base
 from .v4 import primitive_generators
 from .v4 import custom_generator
 from .v4 import derived_generators
+from .v4 import set_special_methods
 
 __all__ = base.__all__ + primitive_generators.__all__ + derived_generators.__all__ + custom_generator.__all__ + ['tohu_logger']
 
