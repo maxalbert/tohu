accaaf14c49d177f767223e3934ebde1f8016419
Maximilian Albert <maximilian.albert@gmail.com>
2018-11-10 15:42:13 +0000

    Ensure every tohu generator has a seed generator and it is reset automatically

--- tohu/v5/base.py
+++ tohu/v5/base.py
@@ -1,7 +1,41 @@
 import hashlib
 from abc import ABCMeta, abstractmethod
+from random import Random
 
 from .logging import logger
+
+__all__ = ['SeedGenerator', 'TohuBaseGenerator']
+
+
+class SeedGenerator:
+    """
+    This class is used in custom generators to create a collection of
+    seeds when reset() is called, so that each of the constituent
+    generators can be re-initialised with a different seed in a
+    reproducible way.
+
+    Note: This is almost identical to the `tohu.Integer` generator, but we
+    need a version which does *not* inherit from `TohuUltraBaseGenerator`,
+    otherwise the automatic item class creation in `CustomGeneratorMeta`
+    gets confused.
+    """
+
+    def __init__(self):
+        self.randgen = Random()
+        self.minval = 0
+        self.maxval = 2**32 - 1
+
+    def reset(self, seed):
+        self.randgen.seed(seed)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.randgen.randint(self.minval, self.maxval)
+
+    def _set_random_state_from(self, other):
+        self.randgen.setstate(other.randgen.getstate())
 
 
 class TohuBaseGenerator(metaclass=ABCMeta):
@@ -11,6 +45,7 @@ class TohuBaseGenerator(metaclass=ABCMeta):
 
     def __init__(self):
         self.tohu_name = None
+        self.seed_generator = SeedGenerator()
 
     def __repr__(self):
         clsname = self.__class__.__name__
@@ -48,5 +83,6 @@ class TohuBaseGenerator(metaclass=ABCMeta):
     @abstractmethod
     def reset(self, seed):
         logger.debug(f'Resetting {self} (seed={seed})')
+        self.seed_generator.reset(seed)
 
         # TODO: reset clones once we have added them back in
