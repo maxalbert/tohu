190f2cb42b74045ff7ec8379329a579ef2c60e39
Maximilian Albert <maximilian.albert@gmail.com>
2018-11-10 15:42:30 +0000

    Add Boolean generator

--- tohu/v5/primitive_generators.py
+++ tohu/v5/primitive_generators.py
@@ -1,6 +1,7 @@
+from random import Random
 from .base import TohuBaseGenerator
 
-__all__ = ['Constant', 'PrimitiveGenerator']
+__all__ = ['Boolean', 'Constant', 'PrimitiveGenerator']
 
 
 class PrimitiveGenerator(TohuBaseGenerator):
@@ -36,3 +37,31 @@ class Constant(PrimitiveGenerator):
 
     def _set_random_state_from(self, other):
         pass
+
+
+class Boolean(PrimitiveGenerator):
+    """
+    Generator which produces random boolean values (True or False).
+    """
+
+    def __init__(self, p=0.5):
+        """
+        Parameters
+        ----------
+        p: float
+            The probability that True is returned. Must be between 0.0 and 1.0.
+        """
+        super().__init__()
+        self.p = p
+        self.randgen = Random()
+
+    def reset(self, seed):
+        super().reset(seed)
+        self.randgen.seed(next(self.seed_generator))
+        return self
+
+    def _set_random_state_from(self, other):
+        self.randgen.setstate(other.randgen.getstate())
+
+    def __next__(self):
+        return self.randgen.random() < self.p
