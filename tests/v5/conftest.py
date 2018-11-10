97fbf474df39da40f282187d8effa0e208b25aff
Maximilian Albert <maximilian.albert@gmail.com>
2018-11-10 23:30:05 +0000

    Remove fixtures

--- tests/v5/conftest.py
+++ tests/v5/conftest.py
@@ -1,5 +1,3 @@
-import pytest
-
 from .context import tohu
 from tohu.v5.primitive_generators import *
 from tohu.v5.logging import logger
@@ -12,20 +10,4 @@ EXEMPLAR_PRIMITIVE_GENERATORS = [
     Boolean(p=0.3),
 ]
 
-
-@pytest.fixture
-def exemplar_generators():
-    """
-    Return a list of generators which contains an example
-    for each type of generator supported by tohu.
-    """
-    return EXEMPLAR_PRIMITIVE_GENERATORS
-
-
-@pytest.fixture
-def exemplar_primitive_generators():
-    """
-    Return a list of generators which contains an example
-    for each type of generator supported by tohu.
-    """
-    return EXEMPLAR_PRIMITIVE_GENERATORS
+EXEMPLAR_GENERATORS = EXEMPLAR_PRIMITIVE_GENERATORS
--- tests/v5/test_reset.py
+++ tests/v5/test_reset.py
@@ -1,15 +1,17 @@
+import pytest
 from unittest.mock import Mock
+from .conftest import EXEMPLAR_GENERATORS
 
 
-def test_reset_automatically_resets_internal_seed_generator(exemplar_generators):
+@pytest.mark.parametrize("g", EXEMPLAR_GENERATORS)
+def test_reset_automatically_resets_internal_seed_generator(g):
     """
     Calling reset on a generator automatically calls reset on its internal seed generator with the same seed.
     """
-    for g in exemplar_generators:
-        g.seed_generator.reset = Mock()
+    g.seed_generator.reset = Mock()
 
-        g.reset(seed=12345)
-        g.seed_generator.reset.assert_called_once_with(12345)
+    g.reset(seed=12345)
+    g.seed_generator.reset.assert_called_once_with(12345)
 
-        g.reset(seed=99999)
-        g.seed_generator.reset.assert_called_with(99999)
+    g.reset(seed=99999)
+    g.seed_generator.reset.assert_called_with(99999)
