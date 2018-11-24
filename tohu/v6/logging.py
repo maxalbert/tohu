b0f14c0026f18913c393361c49f2219ed9ac8659
Maximilian Albert <maximilian.albert@gmail.com>
2018-11-24 14:00:49 +0100

    Fix version in logger

--- tohu/v6/logging.py
+++ tohu/v6/logging.py
@@ -5,7 +5,7 @@ __all__ = ['logger']
 #
 # Create logger
 #
-logger = logging.getLogger('tohu_v5')
+logger = logging.getLogger('tohu_v6')
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
