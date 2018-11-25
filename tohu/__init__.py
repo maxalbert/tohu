5b2a63706d2f9d2853ba1f6ad8d1cf80f8c07676
Maximilian Albert <maximilian.albert@gmail.com>
2018-11-25 14:44:20 +0100

    Check Python version at startup

--- tohu/__init__.py
+++ tohu/__init__.py
@@ -1,3 +1,15 @@
+from distutils.version import StrictVersion
+from platform import python_version
+
+min_supported_python_version = '3.6'
+
+if StrictVersion(python_version()) < StrictVersion(min_supported_python_version):
+    error_msg = (
+        "Tohu requires Python {min_supported_python_version} or greater to run "
+        "(currently running under Python {python_version()})"
+    )
+    raise RuntimeError(error_msg)
+
 from .v4.base import *
 from .v4.primitive_generators import *
 from .v4.derived_generators import *
