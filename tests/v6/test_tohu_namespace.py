8c68662a8ce32cc173932793c34154646002f25e
Maximilian Albert <maximilian.albert@gmail.com>
2018-12-22 18:12:01 +0100

    New stub for TohuNamespace

--- tests/v6/test_tohu_namespace.py
+++ tests/v6/test_tohu_namespace.py
@@ -1,239 +1,95 @@
 import pytest
 
-from unittest.mock import Mock
-
 from .context import tohu
 from tohu.v6.tohu_namespace import TohuNamespace, TohuNamespaceError
 from tohu.v6.primitive_generators import Constant, Integer, HashDigest
 from tohu.v6.derived_generators import Apply, Lookup, SelectMultiple
 
 
-def test_add_generators_with_explicit_names():
-    """
-    Generators can be added to the namespace with explicit names.
-    """
-    g1 = Integer(100, 200)
-    g2 = HashDigest(length=8)
+@pytest.fixture
+def g1():
+    return Integer(100, 200).set_tohu_name("g1")
 
+
+@pytest.fixture
+def g2():
+    return HashDigest(length=6).set_tohu_name("g2")
+
+
+@pytest.fixture
+def h1():
+    return Integer(300, 400).set_tohu_name("h1")
+
+
+@pytest.fixture
+def h2():
+    return HashDigest(length=8).set_tohu_name("h2")
+
+
+def test_new_namespace_is_empty():
+    ns = TohuNamespace()
+    assert len(ns) == 0
+
+
+def test_add_named_generators(g1, g2):
     ns = TohuNamespace()
 
-    assert "aa" not in ns
-    assert "bb" not in ns
-
     assert len(ns) == 0
-    assert [] == ns.keys()
+    assert g1 not in ns
+    assert g2 not in ns
 
-    ns.add_generator(g1, name="aa")
+    ns.add_with_name(g1, "bb")
+
     assert len(ns) == 1
-    assert ["aa"] == ns.keys()
+    assert g1 in ns
+    assert g2 not in ns
 
-    ns.add_generator(g2, name="bb")
+    ns.add_with_name(g2, "aa")
+
     assert len(ns) == 2
-    assert ["aa", "bb"] == ns.keys()
-
-    assert ns["aa"] is g1
-    assert ns["bb"] is g2
+    assert g1 in ns
+    assert g2 in ns
 
 
-def test_add_anonymous_generator():
-    """
-    Generators can be added to the namespace without an explicit name.
-    """
-    g1 = Integer(100, 200).set_tohu_name("g1")
-    g2 = HashDigest(length=8).set_tohu_name("g2")
-
-    tohu_id_g1 = g1.tohu_id
-    tohu_id_g2 = g2.tohu_id
-
+def test_add_anonymous_generators(h1, h2):
     ns = TohuNamespace()
 
-    # assert "aa" not in ns
-    # assert "bb" not in ns
-
     assert len(ns) == 0
-    ns.add_generator(g1, name=None)
+    assert h1 not in ns
+    assert h2 not in ns
+
+    ns.add_anonymously(h1)
+
     assert len(ns) == 1
-    ns.add_generator(g2, name=None)
+    assert h1 in ns
+    assert h2 not in ns
+
+    ns.add_anonymously(h2)
+
     assert len(ns) == 2
-
-    names_expected = [
-        f"ANONYMOUS_ANONYMOUS_ANONYMOUS_g1",
-        f"ANONYMOUS_ANONYMOUS_ANONYMOUS_g2",
-    ]
-    assert names_expected == ns.keys()
-
-    assert ns[names_expected[0]] is g1
-    assert ns[names_expected[1]] is g2
+    assert h1 in ns
+    assert h2 in ns
 
 
-def test_adding_generator_twice_with_the_same_name_ignores_the_second_time():
+def test_add_both_named_and_anonymous_generators(g1, g2, h1, h2):
     ns = TohuNamespace()
 
-    g1 = Integer(100, 200).set_tohu_name("g1")
-    g2 = HashDigest(length=8).set_tohu_name("g2")
-    expected_names = ["aa", "ANONYMOUS_ANONYMOUS_ANONYMOUS_g2"]
-
-    assert len(ns) == 0
-
-    # Add generators for the first time
-    ns.add_generator(g1, "aa")
-    ns.add_generator(g2, None)
-    assert 2 == len(ns)
-    assert 2 == len(ns.keys())
-    assert expected_names == ns.keys()
-
-    # Add generator for the second time with the same names.
-    # This should not change anything.
-    ns.add_generator(g1, "aa")
-    ns.add_generator(g2, None)
-    assert 2 == len(ns)
-    assert 2 == len(ns.keys())
-    assert expected_names == ns.keys()
-
-
-def test_adding_generators_twice_with_different_names_raises_error():
-    ns = TohuNamespace()
-    g1 = Integer(100, 200).set_tohu_name("g1")
-    g2 = HashDigest(length=8).set_tohu_name("g2")
-    expected_error_msg = "because it already exists with a different name"
-
-    assert len(ns) == 0
-
-    # Add g1 for the first time (with an explicit name)
-    ns.add_generator(g1, name="aa")
-    assert ["aa"] == ns.keys()
-
-    # Try to add g1 again with a different name
-    with pytest.raises(TohuNamespaceError, match=expected_error_msg):
-        ns.add_generator(g1, name="aa_new")
-    assert ["aa"] == ns.keys()
-
-    # Adding g1 again anonymously should succeed but not add the generator again
-    ns.add_generator(g1, name=None)
-    assert ["aa"] == ns.keys()
-
-    # Add g2 for the first time (anonymously)
-    ns.add_generator(g2, name=None)
-    assert ["aa", "ANONYMOUS_ANONYMOUS_ANONYMOUS_g2"] == ns.keys()
-
-    # Adding g2 again with an explicit name should update the name
-    ns.add_generator(g2, name="g2_new")
-    assert ["aa", "g2_new"] == ns.keys()
-
-
-def test_adding_derived_generators_also_adds_their_input_generators():
-    ns = TohuNamespace()
-
-    a = Integer(100, 200).set_tohu_name("a")
-    b = Integer(300, 400).set_tohu_name("b")
-    g = Apply(lambda x, y: x + y, a, b).set_tohu_name("g")
-
-    assert len(ns) == 0
-
-    # Add g, which should implicitly also add its input generators a and b
-    ns.add_generator(g, "g")
-
-    names_expected = ['ANONYMOUS_ANONYMOUS_ANONYMOUS_a', 'ANONYMOUS_ANONYMOUS_ANONYMOUS_b', 'g']
+    ns.add_with_name(g1, name="bb")
+    assert len(ns) == 1
+    ns.add_anonymously(h1)
+    assert len(ns) == 2
+    ns.add_with_name(g2, name="aa")
     assert len(ns) == 3
-    assert names_expected == ns.keys()
+    ns.add_anonymously(h2)
+    assert len(ns) == 4
 
 
-def test_adding_generators_with_complex_dependencies():
+def test_retrieve_named_generators(g1, g2, h1, h2):
     ns = TohuNamespace()
 
-    mapping = {
-        1: ['a', 'aa', 'aaa', 'aaaa', 'aaaaa'],
-        2: ['b', 'bb', 'bbb', 'bbbb', 'bbbbb'],
-        3: ['c', 'cc', 'ccc', 'cccc', 'ccccc'],
-    }
-
-    n_vals = Integer(1, 5)
-    g = SelectMultiple(
-        Lookup(
-            g=Integer(1, 3).set_tohu_name("key_gen"),
-            mapping=Constant(mapping).set_tohu_name("mapping")
-        ).set_tohu_name("lookup"),
-        num=n_vals.set_tohu_name("n_vals")
-    )
-
-    ns.add_generator(n_vals, name="aa")
-    ns.add_generator(g, name="bb")
-
-    names_expected = [
-        "aa",
-        "ANONYMOUS_ANONYMOUS_ANONYMOUS_key_gen",
-        "ANONYMOUS_ANONYMOUS_ANONYMOUS_mapping",
-        "ANONYMOUS_ANONYMOUS_ANONYMOUS_lookup",
-        "bb"
-    ]
-    assert 5 == len(ns)
-    assert names_expected == ns.keys()
-
-
-def test_initialisation_from_dictionary():
-    g1 = Integer(1, 5).set_tohu_name("g1")
-    g2 = Constant(['a', 'b', 'c', 'd', 'e']).set_tohu_name("g2")
-    g3 = SelectMultiple(values=g2, num=g1).set_tohu_name("g3")
-
-    ns_dict = {"aa": g1, "bb": 12345, "cc": g3, "dd": "foobar"}
-    ns = TohuNamespace.from_dict(ns_dict)
-
-    expected_names = ["aa", "ANONYMOUS_ANONYMOUS_ANONYMOUS_g2", "cc"]
-    assert 3 == len(ns)
-    assert expected_names == ns.keys()
-
-
-def test_resetting():
-    g1 = Integer(1, 5).set_tohu_name("g1")
-    g2 = Constant(['a', 'b', 'c', 'd', 'e']).set_tohu_name("g2")
-    g3 = SelectMultiple(values=g2, num=g1).set_tohu_name("g3")
-
-    ns = TohuNamespace()
-    ns.add_generator(g1, name="aa")
-    ns.add_generator(g3, name="bb")
-
-    g1.reset = Mock(side_effect=g1.reset)
-    g2.reset = Mock(side_effect=g2.reset)
-    g3.reset = Mock(side_effect=g3.reset)
-
-    ns.reset(seed=12345)
-    g1.reset.assert_called_once_with(seed=831769172)
-    g2.reset.assert_called_once_with(seed=2430986565)
-    g3.reset.assert_called_once_with(seed=694443915)
-
-
-def test_names_vs_keys():
-    g1 = Integer(1, 5).set_tohu_name("g1")
-    g2 = Constant(['a', 'b', 'c', 'd', 'e']).set_tohu_name("g2")
-    g3 = SelectMultiple(values=g2, num=g1).set_tohu_name("g3")
-
-    ns = TohuNamespace()
-    ns.add_generator(g1, name="cc")
-    ns.add_generator(g3, name="aa")
-
-    assert ["cc", "aa"] == ns.names
-    assert ["cc", "ANONYMOUS_ANONYMOUS_ANONYMOUS_g2", "aa"] == ns.keys()
-
-
-def test_named_generators():
-    g1 = Integer(1, 5).set_tohu_name("g1")
-    g2 = Constant(['a', 'b', 'c', 'd', 'e']).set_tohu_name("g2")
-    g3 = SelectMultiple(values=g2, num=g1).set_tohu_name("g3")
-
-    ns = TohuNamespace()
-    ns.add_generator(g1, name="aa")
-    ns.add_generator(g3, name="bb")
-
-    assert ns.named_generators == {"aa": g1, "bb": g3}
-
-
-def test_get_name_for_generator():
-    g1 = Integer(100, 200).set_tohu_name("g1")
-    g2 = HashDigest(length=8).set_tohu_name("g2")
-
-    ns = TohuNamespace()
-    ns.add_generator(g1, name="aa")
-    ns.add_generator(g2, name=None)
-
-    assert ns.get_name(g1) == "aa"
-    assert ns.get_name(g2) == "ANONYMOUS_ANONYMOUS_ANONYMOUS_g2"
+    ns.add_with_name(g1, name="bb")
+    ns.add_anonymously(h1)
+    ns.add_with_name(g2, name="aa")
+    ns.add_anonymously(h2)
+    assert ns["bb"] is g1
+    assert ns["aa"] is g2
--- tohu/v6/tohu_namespace.py
+++ tohu/v6/tohu_namespace.py
@@ -1,131 +1,29 @@
-from bidict import bidict
-
-from .base import TohuBaseGenerator, SeedGenerator
-from .logging import logger
+from .base import TohuBaseGenerator
 
 
 class TohuNamespaceError(Exception):
-    """
-    Custom exception for TohuNamespace.
-    """
-
-
-def get_anonymous_name_for(g):
-    """
-    Return a string that can be used as an anonymous name in a tohu namespace.
-    """
-    if g.tohu_name is not None:
-        return f"ANONYMOUS_ANONYMOUS_ANONYMOUS_{g.tohu_name}"
-    else:
-        return f"ANONYMOUS_ANONYMOUS_ANONYMOUS_{g.__class__.__name__}_{g.tohu_id}"
+    pass
 
 
 class TohuNamespace:
 
     def __init__(self):
-        self.generators = bidict()
-        self.seed_generator = SeedGenerator()
-
-    @classmethod
-    def from_dict(cls, d):
-        ns = cls()
-        ns.update_from_dict(d)
-        return ns
-
-    def update_from_dict(self, d):
-        for name, value in d.items():
-            if isinstance(value, TohuBaseGenerator):
-                self.add_generator(value, name)
-
-    def keys(self):
-        return list(self.generators.keys())
-
-    def items(self):
-        return list(self.generators.items())
-
-    @property
-    def names(self):
-        return [name for name, g in self.generators.items() if not self.is_anonymous(g)]
-
-    @property
-    def named_generators(self):
-        return {name: self.generators[name] for name in self.names}
-
-    def get_name(self, g):
-        try:
-            return self.generators.inv[g]
-        except KeyError:
-            raise KeyError(f"Generator not present in namespace: {g}")
+        self._ns_named = {}
+        self._ns_anonymous = []
 
     def __len__(self):
-        return len(self.generators)
+        return len(self._ns_named) + len(self._ns_anonymous)
 
     def __iter__(self):
-        yield from self.generators.keys()
+        yield from self._ns_named.values()
+        yield from self._ns_anonymous
 
     def __getitem__(self, name):
-        return self.generators[name]
+        return self._ns_named[name]
 
-    def to_str(self):
-        s = ""
-        for name, g in self.generators.items():
-            prefix = "    " if self.is_anonymous(g) else f"{name}: "
-            suffix = " (anonymous)" if self.is_anonymous(g) else ""
-            s += f"{prefix}{g}{suffix}\n"
-        return s
-
-    def contains_generator(self, g):
-        return (g in self.generators.inv)
-
-    def is_anonymous(self, g):
-        try:
-            existing_name = self.generators.inv[g]
-        except KeyError:
-            raise TohuNamespaceError(f"Namespace does not contain generator: {g}")
-
-        return existing_name.startswith("ANONYMOUS_ANONYMOUS_ANONYMOUS_")
-
-    def add_generator(self, g, name):
-        """
-        Add generator `g` to namespace under the given name.
-        """
+    def add_with_name(self, g, name):
         assert isinstance(g, TohuBaseGenerator)
+        self._ns_named[name] = g
 
-        new_name = name or get_anonymous_name_for(g)
-
-        for g_input in g.input_generators:
-            self.add_generator(g_input, name=None)
-
-        if not self.contains_generator(g):
-            self.generators[new_name] = g
-        else:
-            existing_name = self.get_name(g)
-            if new_name == existing_name:
-                logger.debug(f"Not adding generator to namespace because it already exists with the same name: {g}")
-            else:
-                if self.is_anonymous(g):
-                    self.generators.inv[g] = new_name  # update anonymous name with explicit one
-                elif name is None:
-                    pass  # no update needed here (the existing name is already explicit and the new one would be anonymous)
-                else:
-                    raise TohuNamespaceError(
-                        f"Cannot add generator {g} with new name {new_name} because it already exists with a different name: {g}. Existing name: '{existing_name}'")
-
-    def reset(self, seed):
-        self.seed_generator.reset(seed)
-        for g in self.generators.values():
-            g.reset(seed=next(self.seed_generator))
-
-    def _set_random_state_from(self, other):
-        assert isinstance(other, TohuNamespace)
-
-        if self.keys() != other.keys():
-            raise TohuNamespaceError("Namespaces must contain the same keys.")
-
-        for key, g_self in self.generators.items():
-            g_other = other[key]
-
-            if type(g_self) is not type(g_other):
-                raise TohuNamespaceError("Generators in both namespaces must have be of the same types.")
-
-            g_self._set_random_state_from(g_other)
+    def add_anonymously(self, g):
+        self._ns_anonymous.append(g)
