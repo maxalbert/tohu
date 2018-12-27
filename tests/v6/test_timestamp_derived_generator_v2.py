9398da66af4e990cee0700d4e343d1c4f6f57e7f
Maximilian Albert <maximilian.albert@gmail.com>
2018-12-27 17:40:19 +0100

    Allow Constant inputs for start/end/date

--- tests/v6/test_timestamp_derived_generator_v2.py
+++ tests/v6/test_timestamp_derived_generator_v2.py
@@ -4,6 +4,7 @@ import pytest
 from operator import attrgetter
 
 from .context import tohu
+from tohu.v6.primitive_generators import Constant
 from tohu.v6.derived_generators_v2 import TimestampDerived, TohuTimestampError
 
 
@@ -66,7 +67,55 @@ from tohu.v6.derived_generators_v2 import TimestampDerived, TohuTimestampError
         ),
     ],
 )
-def test_expected_start_and_end_value(start, end, date, start_attr, end_attr, start_expected, end_expected):
+def test_expected_start_and_end_value_with_string_inputs(start, end, date, start_attr, end_attr, start_expected, end_expected):
+    g = TimestampDerived(start=start, end=end, date=date)
+
+    assert start_expected == attrgetter(start_attr)(g)
+    assert end_expected == attrgetter(end_attr)(g)
+
+
+@pytest.mark.parametrize(
+    "start, end, date, start_attr, end_attr, start_expected, end_expected",
+    [
+        (
+            Constant("2018-01-01 11:22:33"),
+            Constant("2018-06-28 22:11:02"),
+            None,
+            "start_gen.value",
+            "end_gen.value",
+            dt.datetime(2018, 1, 1, 11, 22, 33),
+            dt.datetime(2018, 6, 28, 22, 11, 2),
+        ),
+        (
+            Constant("2018-01-01 11:22:33"),
+            None,
+            Constant("2018-01-01"),
+            "start_gen.value",
+            "end_gen.value",
+            dt.datetime(2018, 1, 1, 11, 22, 33),
+            dt.datetime(2018, 1, 1, 23, 59, 59),
+        ),
+        (
+            None,
+            Constant("2018-01-01 11:12:13"),
+            Constant("2018-01-01"),
+            "start_gen.value",
+            "end_gen.value",
+            dt.datetime(2018, 1, 1, 0, 0, 0),
+            dt.datetime(2018, 1, 1, 11, 12, 13),
+        ),
+        (
+            None,
+            None,
+            Constant("2018-05-09"),
+            "start_gen.value",
+            "end_gen.value",
+            dt.datetime(2018, 5, 9, 0, 0, 0),
+            dt.datetime(2018, 5, 9, 23, 59, 59),
+        ),
+    ],
+)
+def test_expected_start_and_end_value_with_constant_inputs(start, end, date, start_attr, end_attr, start_expected, end_expected):
     g = TimestampDerived(start=start, end=end, date=date)
 
     assert start_expected == attrgetter(start_attr)(g)
--- tohu/v6/derived_generators_v2.py
+++ tohu/v6/derived_generators_v2.py
@@ -2,25 +2,58 @@ import datetime as dt
 
 from .base import TohuBaseGenerator
 from .primitive_generators import Constant
-from .utils import TohuTimestampError, ensure_is_date_object
+from .utils import TohuDateError, TohuTimestampError, ensure_is_date_object
 
 
-def get_start_end_end_generator(start, end, date):
+def convert_to_date_object(date):
+    if isinstance(date, Constant):
+        return convert_to_date_object(date.value)
+    else:
+        try:
+            return ensure_is_date_object(date)
+        except TohuDateError:
+            raise TohuDateError(f"Cannot convert input to (constant) date object: {date}")
+
+
+def get_start_generator(start, date):
     if date is not None:
-        date = ensure_is_date_object(date)
+        date = convert_to_date_object(date)
 
     if start is None:
         start_value = dt.datetime(date.year, date.month, date.day)
-    else:
+        start_gen = Constant(start_value)
+    elif isinstance(start, str):
         start_value = dt.datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+        start_gen = Constant(start_value)
+    elif isinstance(start, Constant):
+        return get_start_generator(start.value, date)
+    else:
+        raise NotImplementedError()
+
+    return start_gen
+
+
+def get_end_generator(end, date):
+    if date is not None:
+        date = convert_to_date_object(date)
 
     if end is None:
         end_value = dt.datetime(date.year, date.month, date.day, 23, 59, 59)
-    else:
+        end_gen = Constant(end_value)
+    elif isinstance(end, str):
         end_value = dt.datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
+        end_gen = Constant(end_value)
+    elif isinstance(end, Constant):
+        return get_start_generator(end.value, date)
+    else:
+        raise NotImplementedError()
 
-    start_gen = Constant(start_value)
-    end_gen = Constant(end_value)
+    return end_gen
+
+
+def get_start_end_end_generator(start, end, date):
+    start_gen = get_start_generator(start, date)
+    end_gen = get_end_generator(end, date)
     return start_gen, end_gen
 
 
