5a9251a9b69883705ba4532459a0c22705e00c2a
Maximilian Albert <maximilian.albert@gmail.com>
2018-12-27 05:35:35 +0100

    Add a couple of tests for date generator

---
+++ tests/v6/test_date_generator.py
@@ -0,0 +1,23 @@
+import datetime as dt
+
+from .context import tohu
+from tohu.v6.primitive_generators import Date
+
+
+def test_single_date():
+    g = Date(start="2018-01-01", end="2018-01-01")
+    dates = g.generate(100, seed=12345)
+    assert all([x == dt.date(2018, 1, 1) for x in dates])
+
+
+def test_date_range():
+    g = Date(start="1999-11-28", end="1999-12-01")
+    dates = g.generate(1000, seed=12345)
+
+    dates_expected = [
+        dt.date(1999, 11, 28),
+        dt.date(1999, 11, 29),
+        dt.date(1999, 11, 30),
+        dt.date(1999, 12, 1),
+    ]
+    assert set(dates_expected) == set(dates)
