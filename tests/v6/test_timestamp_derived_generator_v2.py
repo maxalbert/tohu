12005fd7071e56ed9b6c4d148b9e34cad8e0c14f
Maximilian Albert <maximilian.albert@gmail.com>
2018-12-27 17:05:18 +0100

    WIP

---
+++ tests/v6/test_timestamp_derived_generator_v2.py
@@ -0,0 +1,9 @@
+import datetime as dt
+import pytest
+
+from operator import attrgetter
+
+from .context import tohu
+from tohu.v6.derived_generators_v2 import TimestampDerived, TohuTimestampError
+
+
