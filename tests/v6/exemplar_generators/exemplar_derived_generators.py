7aacf5e3131f33e062c9a9c4a4c8a9e5fb48b18a
Maximilian Albert <maximilian.albert@gmail.com>
2018-12-27 20:09:50 +0100

    Add exemplar generators for timestamp/date generators

--- tests/v6/exemplar_generators/exemplar_derived_generators.py
+++ tests/v6/exemplar_generators/exemplar_derived_generators.py
@@ -1,6 +1,7 @@
 from ..context import tohu
 from tohu.v6.primitive_generators import *
 from tohu.v6.derived_generators import *
+from tohu.v6.derived_generators_v2 import TimestampDerived
 
 
 def add(x, y):
@@ -11,4 +12,8 @@ EXEMPLAR_DERIVED_GENERATORS = [
     Apply(add, Integer(10, 99).set_tohu_name('xx'), Integer(10, 99).set_tohu_name('yy')),
     Lookup(Integer(1, 5).set_tohu_name('xx'), mapping=Constant({1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'}).set_tohu_name('mm')),
     SelectMultiple(values=['a', 'b', 'c', 'd', 'e'], num=Integer(1, 5)),
+    TimestampDerived(
+        start=TimestampPrimitive(start="2018-01-01 11:22:33", end="2018-03-04 20:19:18"),
+        end=TimestampPrimitive(start="2018-05-06 09:08:07", end="2018-10-01 14:55:33"),
+    )
 ]
--- tests/v6/exemplar_generators/exemplar_primitive_generators.py
+++ tests/v6/exemplar_generators/exemplar_primitive_generators.py
@@ -7,4 +7,6 @@ EXEMPLAR_PRIMITIVE_GENERATORS = [
     Integer(100, 200),
     HashDigest(length=8),
     FakerGenerator(method="name"),
+    TimestampPrimitive(start="2018-01-01 11:22:33", end="2019-04-12 20:00:05"),
+    DatePrimitive(start="1999-04-01", end="2000-05-02"),
 ]
