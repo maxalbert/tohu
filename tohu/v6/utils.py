aae8fed1cef4b3eb9308dfedfb0dce6afaba18c7
Maximilian Albert <maximilian.albert@gmail.com>
2018-12-30 18:31:33 +0100

    Add helper function to explode data frame columns containing sequences into multiple rows for each entry

--- tohu/v6/utils.py
+++ tohu/v6/utils.py
@@ -36,6 +36,46 @@ def make_timestamp_formatter(fmt, uppercase=None):
     return formatter
 
 
+def make_exploded_column(df, colname):
+    """
+    Internal helper function used by `explode_columns()`.
+    """
+    s = df[colname].apply(pd.Series).stack()
+    s.name = colname
+    return s
+
+
+def explode_columns(df, colnames):
+    """
+    Given a dataframe with certain columns that contain lists,
+    return another dataframe where the elements in each list
+    are "exploded" into individual rows.
+
+    Example:
+
+    >>> df
+       col1 col2             col3          col4
+    0   foo   11  [DDD, AAA, CCC]  [dd, aa, cc]
+    1   bar   22            [FFF]          [ff]
+    2  quux   33       [EEE, BBB]      [ee, bb]
+
+    >>> explode_columns(df, ['col3'])
+       col1 col2 col3 col4
+    0   foo   11  DDD   dd
+    1   foo   11  AAA   aa
+    2   foo   11  CCC   cc
+    3   bar   22  FFF   ff
+    4  quux   33  EEE   ee
+    5  quux   33  BBB   bb
+    """
+    remaining_columns = list(df.columns.difference(colnames))
+    df2 = df.set_index(remaining_columns)
+    df3 = pd.concat((make_exploded_column(df2, col) for col in colnames), axis=1)
+    levels_to_reset = list(range(len(remaining_columns)))
+    df4 = df3.reset_index(level=levels_to_reset).reset_index(drop=True)
+    return df4
+
+
 def print_generated_sequence(gen, num, *, sep=", ", fmt='', seed=None):
     """
     Helper function which prints a sequence of `num` items
