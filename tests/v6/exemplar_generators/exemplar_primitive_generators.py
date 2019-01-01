637900c4171ddf6df568754c6832fd2a753e8fc6
Maximilian Albert <maximilian.albert@gmail.com>
2019-01-01 22:52:41 +0100

    Check that each primitive generator class has at least one exemplar generator

--- tests/v6/exemplar_generators/exemplar_primitive_generators.py
+++ tests/v6/exemplar_generators/exemplar_primitive_generators.py
@@ -1,4 +1,7 @@
+from .helpers import check_each_generator_class_has_at_least_one_exemplar_instance
+
 from ..context import tohu
+from tohu.v6.base import PrimitiveGenerator
 from tohu.v6.primitive_generators import Constant, Integer, HashDigest, FakerGenerator, Timestamp, Date
 
 
@@ -10,3 +13,9 @@ EXEMPLAR_PRIMITIVE_GENERATORS = [
     Timestamp(start="2018-01-01 11:22:33", end="2019-04-12 20:00:05"),
     Date(start="1999-04-01", end="2000-05-02"),
 ]
+
+
+check_each_generator_class_has_at_least_one_exemplar_instance(
+    'tohu.v6.primitive_generators',
+    EXEMPLAR_PRIMITIVE_GENERATORS
+)
