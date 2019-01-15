0bd9f04792952a6fa1509522553eae19fa578974
Maximilian Albert <maximilian.albert@gmail.com>
2019-01-15 23:49:24 +0000

    Add sanity check that the relevant operators can be applied to items produced by the input generators

--- tohu/v6/set_special_methods.py
+++ tohu/v6/set_special_methods.py
@@ -13,39 +13,63 @@ from operator import add, mul, gt, ge, lt, le, eq
 __all__ = []
 
 
+def check_that_operator_can_be_applied_to_produces_items(op, g1, g2):
+    """
+    Helper function to check that the operator `op` can be applied to items produced by g1 and g2.
+    """
+    g1_tmp_copy = g1.spawn()
+    g2_tmp_copy = g2.spawn()
+    sample_item_1 = next(g1_tmp_copy)
+    sample_item_2 = next(g2_tmp_copy)
+    try:
+        op(sample_item_1, sample_item_2)
+    except TypeError:
+        raise TypeError(f"Operator '{op.__name__}' cannot be applied to items produced by {g1} and {g2} "
+                        f"(which have type {type(sample_item_1)} and {type(sample_item_2)}, respectively)")
+
+
 def add_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(add, self, other)
     return Apply(add, self, as_tohu_generator(other))
 
 
 def radd_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(add, other, self)
     return Apply(add, as_tohu_generator(other), self)
 
 
 def mul_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(mul, self, other)
     return Apply(mul, self, as_tohu_generator(other))
 
 
 def rmul_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(mul, other, self)
     return Apply(mul, as_tohu_generator(other), self)
 
 
 def eq_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(eq, self, other)
     return Apply(eq, self, as_tohu_generator(other))
 
 
 def lt_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(lt, self, other)
     return Apply(lt, self, as_tohu_generator(other))
 
 
 def le_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(le, self, other)
     return Apply(le, self, as_tohu_generator(other))
 
 
 def gt_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(gt, self, other)
     return Apply(gt, self, as_tohu_generator(other))
 
 
 def ge_generators(self, other):
+    check_that_operator_can_be_applied_to_produces_items(ge, self, other)
     return Apply(ge, self, as_tohu_generator(other))
 
 
