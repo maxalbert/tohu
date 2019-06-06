7e104d446868708118fe060f33ba93777a492696
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-06 21:35:49 +0100

    Add Boolean generator

--- tests/v7/test_primitive_generators.py
+++ tests/v7/test_primitive_generators.py
@@ -1,13 +1,23 @@
 from unittest.mock import Mock
 from .context import tohu
 
-from tohu.v7.primitive_generators import Constant
+from tohu.v7.primitive_generators import Constant, Boolean
 
 
 def test_constant():
     g = Constant("quux")
     expected_values = ["quux", "quux", "quux", "quux", "quux"]
     assert expected_values == g.generate(num=5, seed=99999)
+
+
+def test_boolean():
+    g = Boolean()
+    expected_values = [True, True, False, True, True, True, False, True, True, True, False, True, False, True, False]
+    assert expected_values == g.generate(num=15, seed=12345)
+
+    g = Boolean(p=0.8)
+    expected_values = [True, True, False, True, True, True, True, False, True, False, True, True, True, True, True]
+    assert expected_values == g.generate(num=15, seed=99999)
 
 
 def test_calling_generate_with_and_without_seed():
--- tohu/v7/primitive_generators.py
+++ tohu/v7/primitive_generators.py
@@ -1,3 +1,4 @@
+from random import Random
 from .base import PrimitiveGenerator
 
 
@@ -29,3 +30,31 @@ class Constant(PrimitiveGenerator):
 
     # def _set_state_from(self, other):
     #     pass
+
+
+class Boolean(PrimitiveGenerator):
+    """
+    Generator which produces random boolean values (True or False) with a given probability.
+    """
+
+    def __init__(self, p=0.5):
+        """
+        Parameters
+        ----------
+        p: float
+            The probability that True is returned. Must be between 0.0 and 1.0.
+        """
+        super().__init__()
+        self.p = p
+        self.randgen = Random()
+
+    def reset(self, seed):
+        super().reset(seed)
+        self.randgen.seed(seed)
+        return self
+
+    def __next__(self):
+        return self.randgen.random() < self.p
+
+    # def _set_state_from(self, other):
+    #     self.randgen.setstate(other.randgen.getstate())
