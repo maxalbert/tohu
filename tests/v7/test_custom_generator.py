401839beda030a662777d7ee9363e7584d882da5
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-06 22:34:06 +0100

    Add skeleton for creating custom generators

---
+++ tests/v7/test_custom_generator.py
@@ -0,0 +1,37 @@
+from .context import tohu
+from tohu.v7.primitive_generators import Integer, FakerGenerator, HashDigest
+from tohu.v7.custom_generator import make_new_custom_generator
+
+
+def test_make_custom_generator():
+    g = make_new_custom_generator()
+    assert g.fields == []
+    assert g.field_generators == {}
+
+    aa = Integer(1, 7)
+    g.add_field_generator("aa", aa)
+    assert g.fields == ["aa"]
+    assert g.field_generators == {"aa": aa}
+
+    bb = HashDigest(length=8)
+    g.add_field_generator("bb", bb)
+    assert g.fields == ["aa", "bb"]
+    assert g.field_generators == {"aa": aa, "bb": bb}
+
+    cc = FakerGenerator(method="name")
+    g.add_field_generator("cc", cc)
+    assert g.fields == ["aa", "bb", "cc"]
+    assert g.field_generators == {"aa": aa, "bb": bb, "cc": cc}
+
+
+# def test_custom_generator():
+#
+#     class QuuxGenerator(CustomGenerator):
+#         aa = Integer(1, 7)
+#         bb = HashDigest(length=8)
+#         cc = FakerGenerator(method="name")
+#         dd = Integer(100, 200)
+#
+#     g = QuuxGenerator()
+#     expected_values = []
+#     assert expected_values == g.generate(num=5, seed=99999)
---
+++ tohu/v7/custom_generator.py
@@ -0,0 +1,30 @@
+from .base import TohuBaseGenerator
+
+__all__ = ["make_new_custom_generator"]
+
+
+class CustomGenerator:
+    def __init__(self):
+        self.field_generators = {}
+
+    def add_field_generator(self, name : str, generator : TohuBaseGenerator):
+        """
+        Parameters
+        ----------
+        name : str
+            Field name associated with the generator.
+        generator : TohuBaseGenerator
+            The generator to be added.
+        """
+        self.field_generators[name] = generator
+
+    @property
+    def fields(self):
+        """
+        Return
+        """
+        return list(self.field_generators.keys())
+
+
+def make_new_custom_generator():
+    return CustomGenerator()
