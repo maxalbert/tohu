b09a521f1caeca2a711b7ecfcbda62c412883f66
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-07 22:22:24 +0100

    Revert "Make tohu items comparable if their tohu items classes are equivalent"
    This reverts commit a81b3b877cbd0b3d037271635ecf7bdf29e85c7e.

--- tests/v7/test_tohu_items_class.py
+++ tests/v7/test_tohu_items_class.py
@@ -22,16 +22,17 @@ def test_make_tohu_items_class():
 
 
 
-def test_comparison_between_tohu_item_classes():
+def test_comparison_between_incompatible_tohu_item_classes():
     quux_cls = make_tohu_items_class("Quux", field_names=["foo", "bar", "baz"])
     quux_cls_newly_defined = make_tohu_items_class("Quux", field_names=["foo", "bar", "baz"])
-    quux2_cls = make_tohu_items_class("Quux2", field_names=["foo", "bar", "baz"])
+    quux2_cls = make_tohu_items_class("Quux2", field_names=["aa", "bb"])
 
     item1 = quux_cls(foo=42, bar=True, baz="hello")
     item2 = quux_cls_newly_defined(foo=42, bar=True, baz="hello")
-    item3 = quux2_cls(foo=42, bar=True, baz="hello")
+    item3 = quux2_cls(aa=12, bb=14)
 
-    assert item1 == item2
+    with pytest.raises(TypeError, match="Tohu items have types that cannot be compared"):
+        item1 == item2
 
     with pytest.raises(TypeError, match="Tohu items have types that cannot be compared"):
         item1 == item3
