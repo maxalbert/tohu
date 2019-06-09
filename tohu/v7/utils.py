f41fb4dd5e3971110a597c3d1eb2ddbc39f6f698
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-09 22:55:15 +0100

    Tweak docstring

--- tohu/v7/utils.py
+++ tohu/v7/utils.py
@@ -18,10 +18,14 @@ def identity(x):
     return x
 
 
-def print_generated_sequence(gen, num, *, sep=", ", fmt="", seed=None):  # pragma: no cover
+def print_generated_sequence(gen, num, *, fmt="", sep=", ", seed=None):  # pragma: no cover
     """
-    Helper function which prints a sequence of `num` items
-    produced by the random generator `gen`.
+    Helper function which prints a sequence of `num` item produced by the random generator `gen`.
+
+    Example:
+    >>> g = Integer(1, 20)
+    >>> print_generated_sequence(g, num=10, seed=99999, fmt='02d', sep=", ")
+    Generated sequence: 06, 07, 18, 12, 19, 05, 17, 15, 09, 18
     """
     if seed:
         gen.reset(seed)
