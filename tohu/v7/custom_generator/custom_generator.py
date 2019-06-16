76e671b439ae5d8abd329f6411846a036cc284cd
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-16 02:16:31 +0100

    Automatically populate field generators in custom generators

--- tohu/v7/custom_generator/custom_generator.py
+++ tohu/v7/custom_generator/custom_generator.py
@@ -1,10 +1,43 @@
+from abc import ABCMeta
+
 from ..base import TohuBaseGenerator
 from .tohu_namespace import TohuNamespace
 
 __all__ = ["CustomGenerator"]
 
 
-class CustomGenerator(TohuBaseGenerator):
+def augment_init_method(cls):
+    """
+    Replace the existing cls.__init__() method with a new one which
+    also initialises the field generators and similar bookkeeping.
+    """
+
+    orig_init = cls.__init__
+
+    def new_init(self, *args, **kwargs):
+        super(cls, self).__init__()  # TODO: does this behave correctly with longer inheritance chains? I think so...(?)
+
+        orig_init(self, *args, **kwargs)
+        self.ns.update_from_dict(self.__class__.__dict__)
+        self.ns.update_from_dict(self.__dict__)
+
+    cls.__init__ = new_init
+
+
+
+class CustomGeneratorMeta(ABCMeta):
+
+    def __new__(metacls, cg_name, bases, clsdict):
+        # Create new custom generator class
+        new_cls = super(CustomGeneratorMeta, metacls).__new__(metacls, cg_name, bases, clsdict)
+
+        # Augment original init method with bookkeeping needed for custom generators
+        augment_init_method(new_cls)
+
+        return new_cls
+
+
+class CustomGenerator(TohuBaseGenerator, metaclass=CustomGeneratorMeta):
     def __init__(self, tohu_items_name=None):
         super().__init__()
         tohu_items_name = tohu_items_name or self._get_tohu_items_name()
