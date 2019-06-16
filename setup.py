c7afb42d95f5914b2b92ff1f9fea7d5d1f4a901b
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-16 11:58:13 +0100

    Reformatting

--- setup.py
+++ setup.py
@@ -7,33 +7,30 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Read long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='tohu',
+    name="tohu",
     version=versioneer.get_version(),
-    description='Create random data in a controllable way',
+    description="Create random data in a controllable way",
     long_description=long_description,
-    url='https://github.com/maxalbert/tohu',
-    author='Maximilian Albert',
-    author_email='maximilian.albert@gmail.com',
-    license='MIT',
+    url="https://github.com/maxalbert/tohu",
+    author="Maximilian Albert",
+    author_email="maximilian.albert@gmail.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Utilities',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-    packages=['tohu', 'tohu/v4', 'tohu/v6', 'tohu/v6/custom_generator/', 'tohu/v7'],
-    install_requires=['attrs', 'bidict', 'faker', 'geojson', 'pandas', 'psycopg2-binary', 'shapely', 'sqlalchemy', 'tqdm'],
-    extras_require={
-        'dev': ['ipython', 'jupyter'],
-        'test': ['pytest', 'nbval'],
-    },
+    packages=["tohu", "tohu/v4", "tohu/v6", "tohu/v6/custom_generator/", "tohu/v7"],
+    install_requires=["attrs", "bidict", "faker", "geojson", "pandas", "psycopg2-binary", "shapely", "sqlalchemy", "tqdm"],
+    extras_require={"dev": ["ipython", "jupyter"], "test": ["pytest", "nbval"]},
     cmdclass=versioneer.get_cmdclass(),
-    )
+)
